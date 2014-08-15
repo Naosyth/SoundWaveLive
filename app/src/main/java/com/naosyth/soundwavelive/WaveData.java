b58577d59f9adb5f2afe5866fc5028fa1898d68e
Brandon Worl <brandon.worl@originate.com>
2014-08-15 16:32:25 -0700

    delta time animation

--- app/src/main/java/com/naosyth/soundwavelive/WaveData.java
+++ app/src/main/java/com/naosyth/soundwavelive/WaveData.java
@@ -1,16 +1,27 @@
 package com.naosyth.soundwavelive;
 
+import android.os.SystemClock;
+
 public class WaveData {
     private short[] data;
+
+    private long lastFrameTime = SystemClock.elapsedRealtime();
 
     public WaveData(int sizeInShorts) {
         data = new short[sizeInShorts];
     }
 
     public void addData(short[] newData) {
+        long currentTime = SystemClock.elapsedRealtime();
+        long dt = currentTime - lastFrameTime;
+        lastFrameTime = currentTime;
+
         int index = 0;
         float factor = 44100.0f/data.length;
         int numNew = (int)(newData.length/factor);
+
+        numNew = Math.min((int)(newData.length*(dt/1000.0f)), newData.length)/5;
+        factor = newData.length/numNew;
 
         for (index = 0; index < data.length-numNew; index++) {
             data[index] = data[index+numNew];
